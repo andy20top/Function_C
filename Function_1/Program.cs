aea609d6f5ad42e01d4b27c621e479fcd2e6d84d
Andrey <gusev2097@mail.ru>
2023-07-29 11:40:08 +0500

    Create first fuction

--- Function_1/Program.cs
+++ Function_1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3) 
+{
+    int result = arg1; 
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int max1 = Max(3, 4, 15);
+Console.WriteLine(max1);
